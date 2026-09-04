14940b91e491ec114e40579dd6de0b483e435319
andrey-ivanov-eng1998x8 <279199432+andrey-ivanov-eng1998x8@users.noreply.github.com>
2026-09-04 17:49:35 +0300

    pkg init

---
+++ tg_signal_copier/__init__.py
@@ -0,0 +1,3 @@
+"""Telegram signal scraper and parser daemon."""
+
+__version__ = "0.1.0"
---
+++ tg_signal_copier/__main__.py
@@ -0,0 +1,5 @@
+import sys
+from tg_signal_copier.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main())
